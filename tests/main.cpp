91636b3a3208f6a6d23d53e284000a961a61ed15
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-05-26 14:36:19 +0300

    Add syntactic sugar for pipeline declaration

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,39 @@
 #include <gtest/gtest.h>
 #include <mr-importer/importer.hpp>
 
+#define MR_DECLARE_IMPORT_PIPELINE_BEGIN(Type, ...) \
+namespace mr { \
+  template <> \
+    struct TaskPrototypeBuilder<Type, __VA_ARGS__> { \
+      inline static auto& create()
+
+#define MR_DECLARE_IMPORT_PIPELINE_END \
+  }; \
+}
+
 inline static auto f1 = [] (int x) -> int { return x+1; };
 inline static auto f2 = [] (int x) -> int { return x+1; };
 inline static auto f3 = [] (int x) -> int { return x+1; };
 
+MR_DECLARE_IMPORT_PIPELINE_BEGIN(int, int)
+{
+  static auto prototype = mr::Sequence {
+    f1, f2, f3
+  };
+  return prototype;
+}
+MR_DECLARE_IMPORT_PIPELINE_END
+
+MR_DECLARE_IMPORT_PIPELINE_BEGIN(float, int)
+{
+  static auto prototype = mr::Sequence {
+    f1, f2, f3, [](int x) -> float { return x; }
+  };
+  return prototype;
+}
+MR_DECLARE_IMPORT_PIPELINE_END
+
+#if 0
 namespace mr {
   template <>
     struct TaskPrototypeBuilder<int, int> {
@@ -25,6 +54,7 @@ namespace mr {
       }
     };
 }
+#endif
 
 TEST(ManagerTestInt, SingleIntResource) {
   mr::Manager<int> &mgr = mr::Manager<int>::get();
